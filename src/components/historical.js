e3ccfb06b1339e4e0694330265f7552f3d0ee814
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-10-22 22:11:22 +1000

    Add Wordcloud to History, update server changes

--- src/components/historical.js
+++ src/components/historical.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import {Container, Row, Col, Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
-import TweetHist from './ui_elements/tweethist';
+import Tweet from './ui_elements/tweet';
+import collect from 'collect.js';
+import { render } from 'react-dom';
+import WordCloud from 'react-d3-cloud';
 
 export class Historical extends Component {
 state = {
     tweets: [],
     keys: [],
+    tweetbody: [],
     totalScore: 0,
     sentiment: ""
   };
 
   componentDidMount() {
       // Call our fetch function below once the component mounts
-      let result = this.fetchTweets();
+      this.fetchTweets();
       //this.setState({keys: result});
       //console.log(this.state.keys);
     
   }
 
   render() {
-      if (this.state.keys.length === 0) {
+    const fontSizeMapper = word => Math.log2(word.value) * 5;
+    const rotate = word => word.value % 360;
+      if (this.state.tweetbody.length === 0) {
           return (
               <div>
                   <h1>Loading</h1>
@@ -39,13 +45,18 @@ state = {
               <Container fluid>
                 <Row>
                   <Col xs="5" sm="5">
-                    <h1> { this.state.sentiment}</h1>
-                    <h2> avrage score : { this.state.totalScore}</h2>
-                    <h3> total tweets fetched : { this.state.tweets.length}</h3>
+                  <h2> Average Score: { this.state.totalScore}</h2>
+                  <h3> Total Tweets Fetched: { this.state.tweets.length}</h3>
+                  <h4>Word Cloud</h4>
+                 <WordCloud
+                    data={this.state.tweetbody}
+                    fontSizeMapper={fontSizeMapper}
+                    rotate={rotate}
+                  />
                   </Col>
                   <Col xs="7" sm="7">
-                    {this.state.keys.map((tweet) => (
-                      <TweetHist tweet={tweet} />
+                  {this.state.tweets.map((tweet) => (
+                      <Tweet tweet={tweet} />
                     ))}
                   </Col>
                 </Row>
@@ -57,12 +68,35 @@ state = {
   }
 
   async fetchTweets() {
+    /*
+    {this.state.keys.map((tweet) => (
+                      <TweetHist tweet={tweet} />
+                    ))}
+                    */
     let getUrl = window.location;
     let baseUrl = getUrl.protocol + "//" + getUrl.host + "/";
     let search = window.location.search;
     let parameters = new URLSearchParams(search);
     const mkeys = await fetch('http://localhost:3405/historical?query=' + parameters.get('query')); // Hardcoded address
     const key2 = await mkeys.json();
-    this.setState({keys: key2});
+    let tweetTXT = [];
+    let tweetLST = [];
+    let tweetSTORE = [];
+    key2.forEach(async (element) => {
+      tweetSTORE.push(JSON.parse(element));
+      const twString = JSON.parse(element).text.split(" ");
+      twString.forEach(element2 => {
+        tweetTXT.push(element2);
+      })
+    })
+    const twColl = collect(tweetTXT);
+    const twDup = twColl.countBy();
+    let finalTWT = [];
+    const twFinal = twDup.all();
+    for (const key in twFinal) {
+      let localJS = {text: key, value: twFinal[key]};
+      finalTWT.push(localJS);
+    }
+    this.setState({tweets: tweetSTORE, tweetbody: finalTWT});
   }
 }
