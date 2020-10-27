acc0a695e3cc1db16749782ed96ee1d3874cc65d
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-10-27 21:15:32 +1000

    Redesign home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
-import {Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
+import React, { Component, PropTypes } from 'react';
+import { withRouter } from "react-router-dom";
+import { createBrowserHistory } from 'history';
+import {Navbar, NavbarBrand, Nav, NavItem, NavLink, Input, Container, Row, Col, ButtonGroup, Button} from 'reactstrap';
 export class Home extends Component {
-    state = {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: "",
+        }
+    }
 
     componentDidMount() {
 
+    }
+
+    handleSearchUpdate = (value) => {
+        this.setState({query: value.target.value})
+    }
+
+    getNewTweets = () => {
+        this.props.history.push("/sentiment?query=" + this.state.query)
+    }
+
+    getHistSentiment = () => {
+        this.props.history.push("/scores?query=" + this.state.query)
+    }
+
+    getHistTweets = () => {
+        this.props.history.push("/historical?query=" + this.state.query)
     }
 
     render() {
@@ -19,16 +42,28 @@ export class Home extends Component {
                         </NavItem>
                     </Nav>
                 </Navbar>
-                
-                <form action="/sentiment" method="get">
-                    <label for="query">Query &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                    <input type='text' placeholder='Search Query' name='query' /> <br />
-                    <label for="count">Count &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                    <input type='text' placeholder='Search Count' name='count' /> <br />
-                    <input type="submit" value="Submit" />
-                </form>
+                <br />
+                <Container fluid>
+                    <Row>
+                        <Col xs="3" sm="3"></Col>
+                        <Col xs="6" sm="6">
+                            <Input type="text" placeholder="Search Query" id="search01" name="Search" value={this.state.query} onChange={this.handleSearchUpdate} ></Input><br />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs="4" sm="4"></Col>
+                        <Col>
+                            <ButtonGroup>
+                                <Button color="primary" onClick={this.getNewTweets}>Get New Tweets</Button>
+                                <Button color="primary" onClick={this.getHistSentiment}>View Historical Sentiment</Button>
+                                <Button color="primary" onClick={this.getHistTweets}>View Historical Tweets</Button>
+                            </ButtonGroup>
+                        </Col>
+                    </Row>
+                </Container>
                 </div>
         )
     }
 
 }
+export default withRouter(Home)
